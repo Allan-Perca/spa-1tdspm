f5c2820cf06b744de7425f23494b74ebc4929377
Allan-Perca <a.percario@hotmail.com>
2023-08-31 10:29:40 -0300

    arquivos criados e img adc

--- spa-1tdspm/src/App.jsx
+++ spa-1tdspm/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Importando dependências e ou arquivos que vamos utilizar e ou referenciar.
+import Cabecalho from "./components/Cabecalho/"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App(){
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        {/* Isso é um comentário */}
+        
+        {/* Vamos criar uma cabeçalho com um header, um h1 com um título e uma lista ul com 3 item com link. */}
+
+      <Cabecalho/>
+
+        {/* Vamos criar uma seção com uma div, com 3 <p></p> e estes <p></p> vão ter 3 linhas de lorem, abaixo dos <p></p> uma img. */}
+        <section>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea consectetur quae odit harum debitis blanditiis ipsa quo. Ut quibusdam repellat similique velit libero consequatur, atque aliquid perspiciatis blanditiis, esse at!</p>
+            <p>Magnam ratione quo cupiditate nisi voluptatem accusantium consequuntur sint libero porro, ut, atque quas quaerat distinctio sunt ipsa minima, odio quisquam nam unde rerum dolores amet earum! Quo, amet blanditiis.</p>
+            <p>Ipsa esse beatae quia facilis recusandae. Ullam soluta nemo, hic optio dolorum corporis iste laborum perspiciatis libero itaque animi explicabo quia. Vel dolorem sint corporis, voluptate debitis recusandae quae voluptatem?</p>
+            <img src="" alt="" />
+          </div>
+        </section>
+
+        {/* Vamos criar um rodapé com uma div, uma lista com 3 itens com links para redes sociais e abaixo um <p></p> com o texto e código do símbolo de direitos reservados. */}
+        <footer>
+          <div>
+            <ul>
+              <li><a href="#">GitHub</a></li>
+              <li><a href="#">X</a></li>
+              <li><a href="#">Reddit</a></li>
+            </ul>
+            <p>&copy; Todos os direitos reservados - 2023</p>
+          </div>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
---
+++ spa-1tdspm/src/components/Carros.jsx
@@ -0,0 +1,11 @@
+export default function Carros() {
+    return (
+        <>
+        <ul>
+            <li><img src="" alt="" /></li>
+            <li><img src="" alt="" /></li>
+            <li><img src="" alt="" /></li>
+        </ul>
+        </>
+    );
+}
