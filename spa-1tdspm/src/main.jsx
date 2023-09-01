1789ae1455a6ea4df5abca1abceaa9b1055cafb2
Allan-Perca <a.percario@hotmail.com>
2023-09-01 09:05:56 -0300

    Iniciando a criação de rotas

--- spa-1tdspm/src/main.jsx
+++ spa-1tdspm/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import EditarProdutos from "./routes/EditarProdutos"
+import Erro404 from "./routes/Erro404"
+import Home from "./routes/Home"
+import Produtos from "./routes/Produtos"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
